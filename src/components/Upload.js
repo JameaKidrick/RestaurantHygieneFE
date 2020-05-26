081d6822c0d3f877b0632ee648fc59b88e1cfd13
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-05-25 21:20:54 -0400

    started getting upload to work

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 
 const Upload = () => {
+  const [pic, setPic] = useState('')
+
+  const handleChanges = e => {
+    setPic(e.target.value)
+  }
+
+  console.log(pic)
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios
+      .post('http://localhost:5555/api/upload', e.target.value)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return(
     <div>
       Welcome to the Upload Page!
+      <form onSubmit={handleSubmit}>
+        <input type='file' id='myFile' name='filename' onChange={handleChanges}/>
+        <input type='submit' />
+      </form>
     </div>
   )
 };
