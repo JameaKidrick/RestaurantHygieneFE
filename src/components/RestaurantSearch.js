0268ed3e3ec26b08a553aa9e9664adb0e97087ea
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-06-05 11:30:05 -0400

    user can now search for restaurants by any address; wip: add validation for city and state since they are required

--- src/components/RestaurantSearch.js
+++ src/components/RestaurantSearch.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input'
 
 // ACTIONS
 import { placeLocator } from '../actions';
@@ -10,24 +9,28 @@ const RestaurantSearch = () => {
   const dispatch = useDispatch();
   const places = useSelector(state => state.googleAPIReducer.places)
   const status = useSelector(state => state.googleAPIReducer.status)
+
+  const usStates = ['State', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
+    'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 
+    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 
+    'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+    'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 
+    'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
   const [value, setValue] = useState('1')
-  const [radius, setRadius] = useState('2000')
   const [phoneNumberValidation, setPhoneNumberValidation] = useState(false)
-  // ALSO ACCEPTS phonenumber (IN INTERNATIONAL FORMAT +1 FOR US)
-  // OPTIONAL // CAN INCLUDE: business_status, formatted_address, geometry, icon,name, permanently_closed, photos, place_id, plus_code, types
-  const [location, setLocation] = useState(`circle:${radius}@39.456219,-77.963913`); // OPTIONAL // ALSO ACCEPTS point:lat,lng
+  const [userLocation, setUserLocation] = useState({})
   const [parameters, setParameters] = useState({
     input: '',
     inputType: '',
     fields:
       'place_id,business_status,geometry,icon,photos,formatted_address,name,opening_hours,rating,types,permanently_closed',
-    location: location,
+    radius: 2000,
+    userLocation: userLocation
   });
 
   const handleChange = (e) => {
-    if(e.target.name === 'radius'){
-      setRadius(e.target.value)
-    }else if(e.target.name === 'phonenumber'){
+    if(e.target.name === 'phonenumber'){
       console.log('phonenumber', e.target.value)
       setValue(e.target.value)
       if(isNaN(Number(value))){
@@ -35,6 +38,8 @@ const RestaurantSearch = () => {
       }else{
         setPhoneNumberValidation(false)
       }
+    }else if(e.target.name === 'userAddress' || e.target.name === 'userCity' || e.target.name === 'userState'){
+      setUserLocation({ ...userLocation, [e.target.name]: e.target.value })
     }else{
       setParameters({ ...parameters, [e.target.name]: e.target.value });
     }
@@ -46,77 +51,97 @@ const RestaurantSearch = () => {
       if(parameters.inputType === 'phonenumber'){
         parameters['input'] = `%2B1${value}`
       }
-      setLocation(`circle:${radius}@39.456219,-77.963913`)
+      parameters['userLocation'] = userLocation
       dispatch(placeLocator(parameters))
     }
   };
 
-  console.log(places, status)
-
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <p>Search restaurant by:</p>
-        <input 
-        type='radio' 
-        name='inputType' 
-        value='textquery' 
-        onChange={handleChange}
-        />
-        <label htmlFor='name'>Name</label>
         <br />
-        <input 
-        type='radio' 
-        name='inputType' 
-        value='textquery' 
+        <input
+        type='text'
+        name='userAddress'
+        placeholder='Address'
         onChange={handleChange}
         />
-        <label htmlFor='address'>Address</label>
+        <input
+        type='text'
+        name='userCity'
+        placeholder='City'
+        onChange={handleChange}
+        required
+        />
+        <select name='userState' onChange={handleChange}>
+          {usStates.map((state, index) => {
+            return(
+              <option key={index} value={state}>{state}</option>
+            )
+          })}
+        </select>
         <br />
-        <input 
-        type='radio' 
-        name='inputType' 
-        value='phonenumber'
-        onChange={handleChange}
-        />
-        <label htmlFor='phonenumber'>Phone Number</label>
-        {parameters.inputType === 'textquery' && (
-          <>
-            <br />
-            <input
-            type='text'
-            placeholder='Enter Name or Address'
-            name='input'
-            onChange={handleChange}
-            />
-          </>
-        )}
-        {parameters.inputType === 'phonenumber' && (
-          <>
-            <br />
-            <p>Searches by phone number are limited to the United States</p>
-            <input
-            type='tel'
-            placeholder='Format: 1234567890'
-            name='phonenumber'
-            minLength='10'
-            maxLength='10'
-            onChange={handleChange}
-            required
-            />
-            <br />
-            <label htmlFor='radius'>Choose radius: </label>
-            <select name='radius' onChange={handleChange}>
-              <option value='2000'>1 mile</option>
-              <option value='5000'>3 miles</option>
-              <option value='10000'>5 miles</option>
-              <option value='20000'>10 miles</option>
-            </select>
-            {phoneNumberValidation && (
-              <p>Invalid phone number</p>
-            )}
-          </>
-        )}
+        <label htmlFor='radius'>Choose radius: </label>
+        <select name='radius' onChange={handleChange}>
+          <option value='2000'>1 mile</option>
+          <option value='5000'>3 miles</option>
+          <option value='10000'>5 miles</option>
+          <option value='20000'>10 miles</option>
+        </select>
+        <div>
+          <p>Search restaurant by:</p>
+          <input 
+          type='radio' 
+          name='inputType' 
+          value='textquery' 
+          onChange={handleChange}
+          />
+          <label htmlFor='name'>Name</label>
+          <br />
+          <input 
+          type='radio' 
+          name='inputType' 
+          value='textquery' 
+          onChange={handleChange}
+          />
+          <label htmlFor='address'>Address</label>
+          <br />
+          <input 
+          type='radio' 
+          name='inputType' 
+          value='phonenumber'
+          onChange={handleChange}
+          />
+          <label htmlFor='phonenumber'>Phone Number</label>
+          {parameters.inputType === 'textquery' ? 
+            <>
+              <br />
+              <input
+              type='text'
+              placeholder='Enter Name or Address'
+              name='input'
+              onChange={handleChange}
+              />
+            </>: parameters.inputType === 'phonenumber' ?
+            <>
+              <br />
+              <p>Searches by phone number are limited to the United States</p>
+              <input
+              type='tel'
+              placeholder='Format: 1234567890'
+              name='phonenumber'
+              minLength='10'
+              maxLength='10'
+              onChange={handleChange}
+              required
+              />
+              <br />
+              {phoneNumberValidation && (
+                <p>Invalid phone number</p>
+              )}
+            </>: false
+          }
+        </div>
         <br />
         <button>Find restaurant</button>
       </form>
@@ -124,11 +149,6 @@ const RestaurantSearch = () => {
         places.map((restaurant, restaurantIndex) => {
           return(
             <div key={restaurantIndex}>
-              {/* {restaurant.photos.map((pic, photoIndex) => {
-                return(
-                  <div key={photoIndex}>{pic.html_attributes}</div>
-                )
-              })} */}
               <img src={restaurant.icon} alt='restaurant icon' />
               <h3>{restaurant.name}</h3>
               {restaurant.opening_hours ?
@@ -141,21 +161,10 @@ const RestaurantSearch = () => {
               <p>Address: {restaurant.formatted_address}</p>
             </div>
           )
-        }) : false
+        }): false
       }
     </div>
   );
 };
 
 export default RestaurantSearch;
-
-/*
-data:
-candidates: []
-status: "ZERO_RESULTS"
-__proto__: Object
-headers: {content-length: "41", content-type: "application/json; charset=utf-8"}
-request: XMLHttpRequest {klIsCORSRequest: true, readyState: 4, timeout: 0, RequestSend: ƒ, onreadystatechange: ƒ, …}
-status: 200
-statusText: "OK"
-*/
