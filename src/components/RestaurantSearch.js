00ef69bfc27736ed457e850e4eeeda641a9d0770
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-06-05 08:05:12 -0400

    added validation for phonenumber input and user can now choose the radius for location lookup

--- src/components/RestaurantSearch.js
+++ src/components/RestaurantSearch.js
@@ -9,10 +9,13 @@ import { placeLocator } from '../actions';
 const RestaurantSearch = () => {
   const dispatch = useDispatch();
   const places = useSelector(state => state.googleAPIReducer.places)
-  const [value, setValue] = useState()
+  const status = useSelector(state => state.googleAPIReducer.status)
+  const [value, setValue] = useState('1')
+  const [radius, setRadius] = useState('2000')
+  const [phoneNumberValidation, setPhoneNumberValidation] = useState(false)
   // ALSO ACCEPTS phonenumber (IN INTERNATIONAL FORMAT +1 FOR US)
   // OPTIONAL // CAN INCLUDE: business_status, formatted_address, geometry, icon,name, permanently_closed, photos, place_id, plus_code, types
-  const [location, setLocation] = useState('circle:2000@47.6918452,-122.2226413'); // OPTIONAL // ALSO ACCEPTS point:lat,lng
+  const [location, setLocation] = useState(`circle:${radius}@39.456219,-77.963913`); // OPTIONAL // ALSO ACCEPTS point:lat,lng
   const [parameters, setParameters] = useState({
     input: '',
     inputType: '',
@@ -21,18 +24,34 @@ const RestaurantSearch = () => {
     location: location,
   });
 
-  console.log(places)
   const handleChange = (e) => {
-    setParameters({ ...parameters, [e.target.name]: e.target.value });
+    if(e.target.name === 'radius'){
+      setRadius(e.target.value)
+    }else if(e.target.name === 'phonenumber'){
+      console.log('phonenumber', e.target.value)
+      setValue(e.target.value)
+      if(isNaN(Number(value))){
+        setPhoneNumberValidation(true)
+      }else{
+        setPhoneNumberValidation(false)
+      }
+    }else{
+      setParameters({ ...parameters, [e.target.name]: e.target.value });
+    }
   };
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(parameters.inputType === 'phonenumber'){
-      parameters['input'] = value
+    if(phoneNumberValidation === false){
+      if(parameters.inputType === 'phonenumber'){
+        parameters['input'] = `%2B1${value}`
+      }
+      setLocation(`circle:${radius}@39.456219,-77.963913`)
+      dispatch(placeLocator(parameters))
     }
-    dispatch(placeLocator(parameters))
   };
+
+  console.log(places, status)
 
   return (
     <div>
@@ -44,7 +63,7 @@ const RestaurantSearch = () => {
         value='textquery' 
         onChange={handleChange}
         />
-        <label for='name'>Name</label>
+        <label htmlFor='name'>Name</label>
         <br />
         <input 
         type='radio' 
@@ -52,7 +71,7 @@ const RestaurantSearch = () => {
         value='textquery' 
         onChange={handleChange}
         />
-        <label for='address'>Address</label>
+        <label htmlFor='address'>Address</label>
         <br />
         <input 
         type='radio' 
@@ -60,18 +79,7 @@ const RestaurantSearch = () => {
         value='phonenumber'
         onChange={handleChange}
         />
-        <label for='phonenumber'>Phone Number</label>
-        {parameters.inputType === 'phonenumber' && (
-          <>
-            <br />
-            <PhoneInput
-            placeholder='Enter phone number'
-            value={value}
-            name='input'
-            onChange={setValue}
-            />
-          </>
-        )}
+        <label htmlFor='phonenumber'>Phone Number</label>
         {parameters.inputType === 'textquery' && (
           <>
             <br />
@@ -83,10 +91,36 @@ const RestaurantSearch = () => {
             />
           </>
         )}
+        {parameters.inputType === 'phonenumber' && (
+          <>
+            <br />
+            <p>Searches by phone number are limited to the United States</p>
+            <input
+            type='tel'
+            placeholder='Format: 1234567890'
+            name='phonenumber'
+            minLength='10'
+            maxLength='10'
+            onChange={handleChange}
+            required
+            />
+            <br />
+            <label htmlFor='radius'>Choose radius: </label>
+            <select name='radius' onChange={handleChange}>
+              <option value='2000'>1 mile</option>
+              <option value='5000'>3 miles</option>
+              <option value='10000'>5 miles</option>
+              <option value='20000'>10 miles</option>
+            </select>
+            {phoneNumberValidation && (
+              <p>Invalid phone number</p>
+            )}
+          </>
+        )}
         <br />
-        <button>TEST</button>
+        <button>Find restaurant</button>
       </form>
-      {places.length > 0 ? 
+      {status === 'ZERO_RESULTS' ? <div>No restaurants found within desired radius</div>: places.length > 0 ? 
         places.map((restaurant, restaurantIndex) => {
           return(
             <div key={restaurantIndex}>
@@ -97,18 +131,31 @@ const RestaurantSearch = () => {
               })} */}
               <img src={restaurant.icon} alt='restaurant icon' />
               <h3>{restaurant.name}</h3>
-              {restaurant.opening_hours['open_now'] === true ? 
-                <h4>Open</h4>: <h4>Closed</h4>
+              {restaurant.opening_hours ?
+                restaurant.opening_hours['open_now'] === true ? 
+                <h4>Open</h4>: <h4>Closed</h4>: false
               }
-              <h5>Rating:{restaurant.rating}</h5>
-              <p>Address:{restaurant.formatted_address}</p>
+              {restaurant.rating && (
+                <h5>Rating: {restaurant.rating}</h5>
+              )}
+              <p>Address: {restaurant.formatted_address}</p>
             </div>
           )
-        })
-        :false
+        }) : false
       }
     </div>
   );
 };
 
 export default RestaurantSearch;
+
+/*
+data:
+candidates: []
+status: "ZERO_RESULTS"
+__proto__: Object
+headers: {content-length: "41", content-type: "application/json; charset=utf-8"}
+request: XMLHttpRequest {klIsCORSRequest: true, readyState: 4, timeout: 0, RequestSend: ƒ, onreadystatechange: ƒ, …}
+status: 200
+statusText: "OK"
+*/
