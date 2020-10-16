64bd548dfd7c4617353e6e0df5e89f4befed2597
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-10-15 20:29:16 -0400

    started home styling

--- src/styles/homeStyling.js
+++ src/styles/homeStyling.js
@@ -1,12 +1,58 @@
 import styled from "styled-components";
-import image from '../images/jonathan-borba-Xohjz2UBxgU-unsplash.jpg'
 
 export const HomePage = styled.div`
   // border: 2px solid red;
+  padding-top: 3rem; 
+  padding-right: 5rem;
 
   img {
-    width: 100%;
-    // height: 50vh;
+    height: 35rem; 
+    width: 45rem; 
+    marginTop: 3rem; 
+    marginLeft: 3rem;
+  }
+
+  #header {
+    font-weight: bold;
+    font-size: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  .container {
+    display: flex;
+  }
+
+  .marketingContent{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .buttonContainer {
+    // border: 2px solid green;
+    display: flex;
+    justify-content: space-around;
+    width: 50%;
+    margin: 0 auto;
+    margin-top: 2rem;
+  }
+
+  .demo {
+    // border: 2px solid black;
+  }
+
+  .first {
+    // border: 2px solid red;
+    // width: 80%;
+    margin: 0 auto;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    border-radius: 1rem;
+  }
+
+  .second {
+    border: 2px solid green;
+    width: 80%;
+    margin: 0 auto;
   }
 
   #palette {
